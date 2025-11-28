96ece962061181a3ab97728a9788c2d6df81bca3
Julius Palaganas <juliuspalaganass@gmail.com>
2025-11-28 23:44:21 +0800

    more polishing, patched the security a bit

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import axios from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+axios.interceptors.response.use((response)=>{
+  return response;
+},
+(error)=>{
+  const isServeDown=error.code==='ERR_NETWORK'||error.message==='Network error';
+  const isUnauthorized=error.response&&(error.response.status===401||error.response.status===403);
+  
+  if(isServerDown||isUnauthorized){
+    sessionStorage.removeItem('user');
+    window.dispatchEvent(new Event('storage-update'));
+    if(window.location.pathname !== '/login'){
+      alert("Session Expired or Server Currently Unavailable. Please log in again.");
+      window.location.href = '/login';
+    }
+  }
+  return Promise.reject(error);
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
